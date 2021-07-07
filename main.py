72424660ff7c9642dd9c3ecf9a61c42ce74f47e3
Daniel Olaszy <daniel.olaszy@gmail.com>
2021-07-07 11:42:28 +0100

    Added drop table function

--- main.py
+++ main.py
@@ -14,28 +14,25 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 headers = {'Authorization': 'Bearer ' + os.getenv('API_ACCESS_TOKEN')}
 
-def createProfessionsTable():
+
+def dropTables(table):
+    dropTable = "DROP TABLE IF EXISTS " + table
+    print("Executing '" + dropTable + "'")
+    mycursor.execute(dropTable)
+    print("Dropping " + table + " table...")
+    mydb.commit()
+
+def professionsTable():
+    dropTables("professions")
+    print("Creating professions table...")
     mycursor.execute("CREATE TABLE `professions` (`id` INT NOT NULL, `name` VARCHAR(45) NOT NULL, PRIMARY KEY (`id`))")
-
-
-def populateProfessionsTable():
     sqlAddProfession = "INSERT INTO `professions` (`id`, `name`) VALUES (%s, %s)"
+    sqlDeleteProfessions = "DELETE FROM `professions` WHERE id IN (2777, 2787, 2791, 2811)"
+    print("Executing mysql queries...")
     mycursor.executemany(sqlAddProfession, apiGetProfessions())
+    mycursor.execute(sqlDeleteProfessions)
     print("Committing to db...")
     mydb.commit()
-    
-
-# def apiGetProfessions():
-#     apiProfessions = {}
-#     print("Getting professions from the blizzard api...")
-#     request = 'https://us.api.blizzard.com/data/wow/profession/index?namespace=static-us'
-#     response = requests.get(request, headers=headers)
-#     jsonResponse = response.json().get('professions')
-#     for profession in jsonResponse:
-#         # print("Populating the dictionary")
-#         apiProfessions[profession["id"]] = profession["name"]["en_US"]
-#     print("Obtained the professions...")
-#     return apiProfessions
 
 def apiGetProfessions():
     apiProfessions = []
@@ -48,4 +45,15 @@ def apiGetProfessions():
     print("Obtained professions...")
     return apiProfessions
 
-populateProfessionsTable()
+def apiGetSkillTiers():
+    apiSkillTiers = []
+    print("Getting professions from the blizzard api...")
+    request = 'https://us.api.blizzard.com/data/wow/profession/index?namespace=static-us'
+    response = requests.get(request, headers=headers)
+    jsonResponse = response.json().get('professions')
+    for profession in jsonResponse:
+        apiProfessions.append(tuple((profession["id"], profession["name"]["en_US"])))
+    print("Obtained professions...")
+    return apiProfessions    
+
+professionsTable()
